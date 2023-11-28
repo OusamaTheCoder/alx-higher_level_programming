f7c7bc7a471e475c86e0eecc330bf805b355ac73
Ousama Oujaber <ndnd15237@gmail.com>
2023-11-28 13:07:08 -0800

    Refactor 8-uppercase.py to use .format() instead of f-string

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
 def uppercase(s):
-    separator = ""
     for char in s:
         ascii_value = ord(char)
         if 97 <= ascii_value <= 122:
             uppercase_char = chr(ascii_value - 32)
         else:
             uppercase_char = char
-        print("{:s}".format(separator), end="")
-        print("{:s}".format(uppercase_char), end="")
-        separator = ""
+        print("{}".format(uppercase_char), end="")
     print()
