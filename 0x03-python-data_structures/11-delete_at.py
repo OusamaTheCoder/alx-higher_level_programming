e3fbd45ac1c35edf9beaaf6c254707a1ab10d32b
Ousama Oujaber <ndnd15237@gmail.com>
2023-12-03 11:17:18 -0800

    python

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+
+    new_list = []
+    for i, element in enumerate(my_list):
+        if i != idx:
+            new_list.append(element)
+
+    return new_list
