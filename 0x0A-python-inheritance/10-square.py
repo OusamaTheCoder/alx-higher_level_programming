d7861dceca25d5f394bae617f92988a35ee73e49
Ousama Oujaber <ndnd15237@gmail.com>
2024-01-08 09:29:21 -0800

    python

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''Class Square.'''
+class Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    '''Class Square.'''
+    def __init__(self, size):
+        '''__init__.'''
+        super().__init__(size, size)
+
+    def __str__(self):
+        '''__str__.'''
+        return f"[Square] {self._Rectangle__width}/{self._Rectangle__height}"
+
+    def area(self):
+        '''area.'''
+        return self._Rectangle__width * self._Rectangle__height
