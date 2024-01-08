aa709f35a7c9a4db17fcf27f7963e1ec49ce6be3
Ousama Oujaber <ndnd15237@gmail.com>
2024-01-08 09:14:17 -0800

    python

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''Rectangle class.'''
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    '''Rectangle class.'''
+    def __init__(self, width, height):
+        '''__init__.'''
+        super().__init__()
+        self.__width = self.integer_validator("width", width)
+        self.__height = self.integer_validator("height", height)
+
+    def area(self):
+        '''Area method.'''
+        return self.__width * self.__height
+
+    def __str__(self):
+        '''__str__.'''
+        return f"[Rectangle] {self.__width}/{self.__height}"
