627f9b04d72c57dfd083bc40efb92e9c5929de1c
Ousama Oujaber <ndnd15237@gmail.com>
2024-01-08 09:16:08 -0800

    python

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -17,4 +17,4 @@ class Rectangle(BaseGeometry):
 
     def __str__(self):
         '''__str__.'''
-        return f"[Rectangle] {self.__width}/{self.__height}"
+        return "[Rectangle] " + str(self.__width) + "/" + str(self.__height)
