b8c7299d694719cf22a471e03782d0ec8daa95df
Ousama Oujaber <ndnd15237@gmail.com>
2024-01-13 03:14:03 -0800

    python

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module for searching and updating a file.
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """
+    Function that inserts a line of text to a file,
+    after each line containing a specific string.
+    """
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+
+    with open(filename, 'w') as file:
+        for line in lines:
+            file.write(line)
+            if search_string in line:
+                file.write(new_string)
