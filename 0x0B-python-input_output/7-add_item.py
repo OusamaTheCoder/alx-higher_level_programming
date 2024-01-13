7daff39a95ce9180710625a292e9c910857301d0
Ousama Oujaber <ndnd15237@gmail.com>
2024-01-13 02:57:51 -0800

    python

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,8 +5,9 @@ to a Python list and saving them to a JSON file.
 """
 import sys
 from os import path
-from 5-save_to_json_file import save_to_json_file
-from 6-load_from_json_file import load_from_json_file
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
 
